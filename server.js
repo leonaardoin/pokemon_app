0b462e6737105ff554f7dcf75dfb016ce70fe691
Léona Ardoin <126014224+leonaardoin@users.noreply.github.com>
2023-05-12 19:35:02 -0400

    Updated

--- server.js
+++ server.js
@@ -1,25 +1,60 @@
 const express = require('express')
-const app = express();
-const port = 3000 
+const app = express()
+const port = 3000
+const Pokemon = require('./models/pokemon.js')
+const mongoose = require('mongoose');
 
-const pokemon = require('./models/pokemon.js');
 
+require('dotenv').config()
 
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.once('open', ()=> {
+    console.log('connected to mongo');
+});
+
+app.use(express.urlencoded({extended:false}));
+
+// Middleware here
 app.set('view engine', 'jsx');
 app.engine('jsx', require('jsx-view-engine').createEngine());
 
-app.get('/', (req, res)=>{
-    res.send('Welcome to the Pokemon App!<br/><br/><a href="http://localhost:3000/pokemon">See All...</a>')
+app.use((req, res, next) => {
+    // console.log('I run for all routes');
+    next();
+});
+
+//routes
+app.get('/', (req, res) => {
+    res.send('Welcome to the Pokemon App!<br></br><a href="/pokemon" >pokemon</a>')
 })
 
 app.get('/pokemon', (req, res)=>{
-    res.render('Index', {pokemon: pokemon})
+    Pokemon.find({}, (error, allPokemon)=>{
+        res.render('Index', {
+            pokemon: allPokemon
+        });
+    });
+});
+
+app.get('/pokemon/new', (req, res) => {
+    res.render('New');
+});
+
+app.post('/pokemon', (req, res)=>{
+    Pokemon.create(req.body, (error, createdPokemon)=>{
+        res.redirect('/pokemon');
+    });
+});
+
+app.get('/pokemon/:id', (req, res) => { 
+      Pokemon.findById(req.params.id, (error, foundPokemon)=>{
+      res.render('Show', { //second param must be an object
+          pokemon:foundPokemon
+        });
+    })
 })
 
-app.get('/pokemon/:id', (req, res)=>{
-    res.render('Show', {pokemon: pokemon[req.params.id]})
-})
 
 app.listen(port, () => {
-    console.log(`Listening to server on, ${port}`)
-});
+    console.log(`Listening on, ${port}`)
+})
--- views/Show.jsx
+++ views/Show.jsx
@@ -1,15 +1,24 @@
+const React = require('react');
+    
+const myStyle = {
+    color: 'red',
+    backgroundColor: 'orange',
+    };
+
 class Show extends React.Component {
     render() {
-      const pokemon = this.props.pokemon
-      return (
-      <div style={myStyle}>
-          <h1>Gotta Catch 'Em All!</h1>
-          <h2>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</h2>
-          <img src={pokemon.img + ".jpg"} /><br/><br/>
-          <a href={'/pokemon'}>Back</a>
-      </div>
-      )
+        const pokemon = this.props.pokemon;
+        return (
+            <div style={myStyle}>
+                <h1>Gotta Catch 'Em All</h1>
+                <h2>
+                    {pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}                                
+                    <img src={`${pokemon.img.concat('.jpg')}`}></img>
+                </h2>
+                <a href="/pokemon"> Back </a>
+            </div>
+        );
     }
-  }
+}
 
-  module.exports = Show
+module.exports = Show;
