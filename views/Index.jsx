5a42a915a7f27b0e15b45013ba83c93db8b35d6f
Léona Ardoin <126014224+leonaardoin@users.noreply.github.com>
2023-05-12 19:36:21 -0400

    Update Index.jsx

--- views/Index.jsx
+++ views/Index.jsx
@@ -1,34 +1,3 @@
-// const React = require('react');
-    
-// const myStyle = {
-//     color: 'red',
-//     backgroundColor: 'orange',
-//     };
-
-//     class Index extends React.Component {
-//         render() {
-//           const { pokemon } = this.props
-//           return (
-//           <div style={myStyle}>
-//               <h1>See All The Pokemon!</h1>
-//               <ul>
-//                 {pokemon.map((pokemon, i) => {
-//                   return (
-//                     <li>
-//                       <a href={`/pokemon/${i}`}>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</a>
-//                     </li>
-//                   );
-//                 })}
-//               </ul>
-//               <nav>
-//                   <a href="http://localhost:3000">Back</a>
-//               </nav>
-//           </div>
-//           )
-//         }
-//       }
-      
-//       module.exports = Index
 
 const React = require('react');
     
